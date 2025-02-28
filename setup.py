43bac0d55ce551176ec094275f0b3d77b2c46ed6
Siddharth Mutkekar <siddharthmutkekar@in.ibm.com>
2025-02-28 11:37:39 +0530

    first commit

--- setup.py
+++ setup.py
@@ -11,7 +11,7 @@ setuptools.setup(
     description="A function that returns 'world'",
     long_description=long_description,
     long_description_content_type="text/markdown",
-    url="https://github.ibm.com/SiddharthMutkekar/python_world",
+    url="https://github.com/SiddharthMutkekar/python_world",
     packages=setuptools.find_packages(),
     classifiers=[
         "Programming Language :: Python :: 3",
